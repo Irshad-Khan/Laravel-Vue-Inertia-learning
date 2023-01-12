5676b79b17aab88a4ec693f789e609d5b7f67210
Irshad-Khan <mcsirshad95@gmail.com>
2023-01-12 10:58:22 +0500

    Complete structure

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>Index View</h3>
+        <Link href="/hello">Show Page</Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>Show View</h3>
+        <Link href="/">Main Page</Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+</script>
